806bcea6f2dadda457e81ed6c77911eefae283c5
arshad-ashraf-a <arshad.ashraf.a@outlook.com>
2017-05-24 20:19:18 +0400

    Authentication API Removed UnWanted Codes

---
+++ Libraries/Takamul.Libraries/Takamul.Models/ApiViewModel/TakamulUserResponse.cs
@@ -0,0 +1,32 @@
+﻿/****************************************************************************************************/
+/* Class Name           : TakamulUser.cs                                                            */
+/* Designed BY          : Arshad Ashraf                                                             */
+/* Created BY           : Arshad Ashraf                                                             */
+/* Creation Date        : 31.03.2017                                                                */
+/* Modified BY          : -                                                                         */
+/* Last Modified Date   : -                                                                         */
+/* Description          : used for hold of user data                                                */
+/****************************************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Takamul.Models.ApiViewModel
+{
+    public class TakamulUserResponse
+    {
+        public TakamulUser TakamulUser { get; set; }
+
+        public ApiResponse ApiResponse { get; set; }
+        #region :: Constructor ::
+        public TakamulUserResponse()
+        {
+            TakamulUser = new TakamulUser();
+            ApiResponse = new ApiResponse();
+        }
+        #endregion
+    }
+}
