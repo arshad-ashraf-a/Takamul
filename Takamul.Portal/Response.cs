a1c4e79157c6f49fbd3b1e5d325c4cae5461c6ee
arshad-ashraf-a <arshad.ashraf.a@outlook.com>
2018-04-30 10:36:18 +0400

    DemoFiles
    DemoFiles Desc

---
+++ Takamul.Portal/Response.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Response
+    {
+        public OperationResult OperationResult { get; set; }
+        public string OperationResultMessage { get; set; }
+
+    }
+    public enum OperationResult
+    {
+        Success = 1,
+        Failed = 0
+    }
+}
